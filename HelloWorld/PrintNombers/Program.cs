82cb68114ee70a9211b3cbd83ca936a902af50ba
veronikapp <ppaskovaveronika@gmail.com>
2017-09-04 18:17:03 +0300

    solved printing number

--- HelloWorld/PrintNombers/Program.cs
+++ HelloWorld/PrintNombers/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace PrintNombers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,C#!");
-
-
         }
     }
 }
